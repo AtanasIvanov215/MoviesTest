091907a5cc2f8afc5d7f88d55f6395d606ef4360
AtanasIvanov215 <83120646+AtanasIvanov215@users.noreply.github.com>
2023-09-11 11:20:26 +0300

    fix: adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pandas_test',
+    version='0.0.1',
+    packages=[''],
+    install_requires=['pandas', 'ast', 'json'],
+    url='',
+    license='',
+    author='atanas.ivanov215',
+    author_email='atanas.ivanov215@gmail.com',
+    description='Pandas'
+)
